a0af8d3aaad2d5108bfb9039a21bf438a9122fd2
Mark Lilback <mark@lilback.com>
2019-09-14 12:55:33 -0400

    stubbed out enough to compile

--- Sources/kappserver/K8sServer.swift
+++ Sources/kappserver/K8sServer.swift
@@ -6,6 +6,14 @@
 //
 
 import Foundation
+import BrightFutures
+
+enum K8sError: Error {
+	case connectionFailed
+	case invalidResponse
+	case impossibleSituation
+	case invalidConfiguration
+}
 
 class K8sServer {
 //	private let token: String
@@ -15,4 +23,13 @@ class K8sServer {
 		self.config = config
 	}
 	
+	/// Fires off a job to kubernetes to start a compute instance for the specified workspace
+	/// - Parameter wspaceId: the id of the workspace the compute engine will be using
+	/// - Parameter sessionId: the unique, per-session id this compute instance is for
+	/// - Returns: future is always true if no error happend
+	/// FIXME: need to delay return value until the compute container is running and accepting connections
+	func launchCompute(wspaceId: Int, sessionId: Int) -> Future<Bool, K8sError> {
+		// TODO: really implement
+		return Future<Bool, K8sError>(value: false, delay: .seconds(2))
+	}
 }
