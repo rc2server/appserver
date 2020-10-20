05c39cdde4b2f539e62ecb7e785c5cac56e13eba
Mark Lilback <mark@lilback.com>
2020-10-19 21:13:56 -0400

    adding CodingKeys for mismatched json names

--- Sources/appcore/ComputeResponse.swift
+++ Sources/appcore/ComputeResponse.swift
@@ -272,6 +272,14 @@ public enum ComputeResponse: Equatable {
 		public let updateIdentifier: String
 		public let results: String
 		
+		enum CodingKeys: String, CodingKey {
+			case previewId
+			case chunkId
+			case updateComplete = "complete"
+			case updateIdentifier
+			case results = "content"
+		}
+
 		public init(previewId: Int, chunkId: Int, results: String, updateIdentifier: String = "", updateComplete: Bool) {
 			self.previewId = previewId
 			self.chunkId = chunkId
