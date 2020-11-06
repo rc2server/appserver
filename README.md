d3dc3bab007166b3cc8d6e57168e5488b0a139cf
Mark Lilback <mark@lilback.com>
2020-11-06 13:44:10 -0500

    updated so test can always be done with docker

--- README.md
+++ README.md
@@ -5,13 +5,17 @@ The appserver for rc2. Uses swift 5.3
 (this was in old version, not sure if still true)
 On ubuntu, requires libcurl3-gnutls-dev and libpq-dev installed.
 
+## building
+
+`swift build` will not work unless you do ` export KITURA_NIO=1` once (or put in .bash_profile)
+
 ## configuration
 
-when running, uses config.json. To run for testing with postgresql on 5432, set `RC2_CONFIG_FILE_NAME=config-test.json`
+when running uses config.json. When not in docker, use to use the debug config `export RC2_CONFIG_FILE_NAME=config-test.json`
 
 ## db setup
 
-if rcuser is empty, run `psql -c "select rc2CreateUser('local', 'Local', 'Account', 'singlesignin@rc2.io', 'local');"`
+if rcuser is empty, run `psql -c "select rc2CreateUser('local', 'Local', 'Account', 'singlesignin@rc2.io', 'local');"` This does not apply if testing with docker
 
 ## Testing Using Docker
 
--- setupDockerForTesting.sh
+++ setupDockerForTesting.sh
@@ -9,17 +9,21 @@ function finish {
 #trap finish EXIT
 PDIR=`pwd`
 SQLFILE="${PDIR}/rc2root/rc2.sql"
+export KITURA_NIO=1
 echo "looking for ${SQLFILE}"
 docker run --name appserver_test -e POSTGRES_PASSWORD="apptest" -p 5434:5432 --rm -d postgres:11
 echo "waiting for db to start"
 sleep 5;
 docker logs appserver_test
 docker exec appserver_test psql -U postgres -c "create database rc2;"
+docker exec appserver_test psql -U postgres -c "create extension if not exists pgcrypto;"
 docker exec appserver_test psql -U postgres -c "create user rc2 superuser password 'secret';"
 docker exec appserver_test psql -U postgres -c "grant all privileges on database rc2 to rc2;"
 docker cp "${SQLFILE}" appserver_test:/tmp/rc2.sql
 docker exec appserver_test psql -U postgres -d rc2 --file=/tmp/rc2.sql
-
+docker exec appserver_test psql -U rc2 -c "select rc2CreateUser('rc2', 'local', 'Account', 'singlesignin@rc2.io', 'local');"
+docker cp testData.pgsql appserver_test:/tmp/testData.sql
+docker exec appserver_test psql -U rc2 -d rc2 -f /tmp/testData.sql 
 sleep 1;
 echo "server ready"
 
