69687b4a583c2961c80a715450b6f3d270a8dd8f
Mark Lilback <mark@lilback.com>
2020-11-07 17:08:35 -0500

    added Queue struct

---
+++ Sources/appcore/Queue.swift
@@ -0,0 +1,40 @@
+/**
+	Taken from [Swift Algorithm Club](https://github.com/raywenderlich/swift-algorithm-club/blob/master/Queue/Queue-Simple.swift)
+
+  First-in first-out queue (FIFO)
+  New elements are added to the end of the queue. Dequeuing pulls elements from
+  the front of the queue.
+  Enqueuing is an O(1) operation, dequeuing is O(n). Note: If the queue had been
+  implemented with a linked list, then both would be O(1).
+*/
+public struct Queue<T> {
+  fileprivate var array = [T]()
+
+  public var count: Int {
+    return array.count
+  }
+
+  public var isEmpty: Bool {
+    return array.isEmpty
+  }
+
+  public mutating func enqueue(_ element: T) {
+    array.append(element)
+  }
+
+  public mutating func dequeue() -> T? {
+    if isEmpty {
+      return nil
+    } else {
+      return array.removeFirst()
+    }
+  }
+
+  public mutating func removeAll() {
+    array.removeAll()
+  }
+
+  public var front: T? {
+    return array.first
+  }
+}
