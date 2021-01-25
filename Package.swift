45b6d673dc2385bc2b9f2613683e8caa1328d4ba
Mark Lilback <mark@lilback.com>
2021-01-25 17:20:41 -0500

    compute worker now uses websockets w/o testing

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
 		.package(url: "https://github.com/Thomvis/BrightFutures.git", from: "8.0.1"),
 		.package(url: "https://github.com/ianpartridge/swift-backtrace.git", from: "1.1.1"),
 		.package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0"),
-		.package(url: "https://github.com/mlilback/SwiftyJSON.git", .branch("master"))
+		.package(url: "https://github.com/mlilback/SwiftyJSON.git", .branch("master")),
+		.package(name: "KituraWebSocketClient", path: "../Kitura-WebSocket-Client")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -39,6 +40,7 @@ let package = Package(
 			.product(name: "Kitura-WebSocket", package: "KituraWebSocket"),
 			 "Rc2Model", "pgswift", "HeliumLogger", 
 			 .product(name: "Logging", package: "swift-log"), 
+			 .product(name: "KituraWebSocketClient", package: "KituraWebSocketClient"),
 			 "CommandLine", "SwiftJWT", "Zip", "FileKit", "Socket", "BrightFutures"]),
         .target(
         	name: "servermodel",
--- Sources/appcore/ComputeWorker.swift
+++ Sources/appcore/ComputeWorker.swift
@@ -9,6 +9,8 @@ import Foundation
 import Logging
 import Socket
 import NIO
+import NIOHTTP1
+import KituraWebSocketClient
 
 /// for owner that needs to get callbacks
 public protocol ComputeWorkerDelegate: class {
@@ -45,11 +47,7 @@ public class ComputeWorker {
 	let sessionId: Int
 	let k8sServer: K8sServer?
 	let eventGroup: EventLoopGroup
-	private var socket: Socket? = nil
-	private var readBuffer: UnsafeMutablePointer<CChar>
-	private var readBufferSize: Int
-	private let readQueue: DispatchQueue
-	private let lock = DispatchSemaphore(value: 1)
+	let wsclient: WebSocketClient
 	
 	private weak var delegate: ComputeWorkerDelegate?
 	private(set) var state: ComputeState = .uninitialized {
@@ -66,22 +64,20 @@ public class ComputeWorker {
 		self.delegate = delegate
 		self.k8sServer = k8sServer
 		self.eventGroup = eventGroup
-		readBufferSize = config.computeReadBufferSize
-		readBuffer = UnsafeMutablePointer<CChar>.allocate(capacity: readBufferSize)
-		readBuffer.initialize(repeating: 0, count: readBufferSize)
-		readQueue = queue ?? DispatchQueue.global(qos: .default)
+		let wsUrl = "ws://\(config.computeHost):9001/";
+		self.wsclient = WebSocketClient(wsUrl, eventLoopGroup: eventGroup)! // code shows no path to return nil
 	}
 
-	deinit {
-		readBuffer.deallocate()
-	}
-	
 	// MARK: - functions
 	
 	public func start() throws {
 		assert(state == .uninitialized, "programmer error: invalid state")
 		guard config.computeViaK8s else {
-			try openConnection(ipAddr: config.computeHost)
+			do {
+				try wsclient.connect()
+			} catch {
+				throw ComputeError.failedToConnect
+			}
 			return
 		}
 		guard k8sServer != nil else { fatalError("programmer error: can't use k8s without a k8s server") }
@@ -91,28 +87,17 @@ public class ComputeWorker {
 	}
 	
 	public func shutdown() throws {
-		guard state == .connected, socket?.isConnected ?? false else {
+		guard state == .connected, wsclient.isConnected else {
 			logger.info("asked to shutdown when not running")
 			throw ComputeError.notConnected
 		}
-		socket?.close()
-		socket = nil
+		wsclient.close()
 	}
 	
 	public func send(data: Data) throws {
 		guard data.count > 0 else { throw ComputeError.sendingEmptyMessage }
-		guard state == .connected, let socket = socket, socket.isConnected else { throw ComputeError.notConnected }
-		var headBytes = [UInt8](repeating: 0, count: 8)
-		headBytes.replaceSubrange(0...3, with: valueByteArray(UInt32(0x21).byteSwapped))
-		headBytes.replaceSubrange(4...7, with: valueByteArray(UInt32(data.count).byteSwapped))
-		let rawData = Data(headBytes) + data
-		logger.info("sending to compute: \(String(data: rawData, encoding: .utf8) ?? "no data" )")
-		do {
-			try socket.write(from: rawData)
-		} catch {
-			logger.warning("failed to write: \(error)")
-			throw ComputeError.failedToWrite
-		}
+		guard state == .connected, wsclient.isConnected else { throw ComputeError.notConnected }
+		wsclient.sendMessage(data: data, opcode: .text)
 	}
 
 	// MARK: - private methods
@@ -129,101 +114,36 @@ public class ComputeWorker {
 		}
 	}
 
-	private func openConnection(ipAddr: String) throws {
-		do {
-			self.state = .connecting
-			socket = try Socket.create()
-			socket?.readBufferSize = 32768 // 32 KB
-			let port = Int32(config.computePort) //kitura uses Int32 for legacy purposes
-			logger.debug("compute connectding to \(config.computeHost):\(port)")
-			try socket?.connect(to: config.computeHost, port: port)
-			logger.debug("compute worker socket open")
-			self.state = .connected
-			readQueue.async { [weak self] in
-				self?.readNext()
-			}
-		} catch {
-			logger.error("error opening socket to compute engine: \(error)")
-			self.state = .failedToConnect
-			throw ComputeError.failedToConnect
-		}
-	}
-	
 	private func updateStatus() {
 		fatalError("not implemented")
 	}
-	
-	private func readNext() {
-		guard let socket = socket, socket.isActive, !socket.remoteConnectionClosed else {
-			if state == .unusable { return } //duplicate time
-			state = .unusable
-			logger.warning("no open socket to read or closed connection")
-			delegate?.handleConnectionClosed()
-			return
-		}
-		let waitStatus = lock.wait(timeout: .now() + .milliseconds(2))
-		// if lock is busy, don't want to queue another read
-		guard waitStatus == .success else { return }
-		defer { lock.signal() }
-		// in any other case, we'll want to read again
-		defer { readQueue.async { [weak self] in self?.readNext() } }
-		// read our magic header
-		let header = UnsafeMutablePointer<CChar>.allocate(capacity: 8)
-		defer { header.deallocate() }
-		header.initialize(repeating: 0, count: 8)
-		do {
-			let readCount = try socket.read(into: header, bufSize: 8, truncate: true)
-			if readCount == 0 {
-				// need to check status of socket
-				if socket.remoteConnectionClosed, state != .unusable {
-					self.state = .unusable
-					self.socket?.close()
-					delegate?.handleConnectionClosed()
-				}
-				return
-			}
-			guard readCount == 8 else {
-				// this could only happen if we connected to an invalid server.
-				// TODO: abort this worker
-				logger.error("failed to read magic header: \(readCount)")
-				return
-			}
-			let anticipatedSize = try verifyMagicHeader(bytes: header)
-			let sizeToRead = readBufferSize > anticipatedSize ? anticipatedSize : readBufferSize
-			// now read size bytes
-			let sizeRead = try socket.read(into: readBuffer, bufSize: sizeToRead, truncate: true)
-			guard sizeRead == sizeToRead else {
-				logger.error("size read from compute (\(sizeRead)) does not match anticipated size (\(anticipatedSize))")
-				return // just throw away what was read
-			}
-			// pass along a Data w/o copying the memory
-			let rawPtr = UnsafeMutableRawPointer(readBuffer)
-			let tmpData = Data(bytesNoCopy: rawPtr, count: sizeRead, deallocator: .none)
-			logger.debug("read from compute: \(tmpData)")
-			readQueue.sync {
-				self.delegate?.handleCompute(data: tmpData)
-			}
-		} catch {
-			logger.error("error reading from compute socket: \(error)")
-			readQueue.async {
-				self.delegate?.handleCompute(error: .failedToReadMessage)
-			}
-		}
-	}
-	
-	private func verifyMagicHeader(bytes: UnsafeMutablePointer<CChar>) throws -> Int {
-		let (header, dataLen) = bytes.withMemoryRebound(to: UInt32.self, capacity: 2) { return (UInt32(bigEndian: $0.pointee), UInt32(bigEndian: $0.advanced(by: 1).pointee))}
-		// tried all kinds of withUnsafePointer & withMemoryRebound and could not figure it out.
-		logger.debug("compute sent \(dataLen) worth of json")
-		guard header == 0x21 else { throw ComputeError.invalidHeader }
-		return Int(dataLen)
-	}
+}
 
-	private func valueByteArray<T>(_ value:T) -> [UInt8] {
-		var data = [UInt8](repeatElement(0, count: MemoryLayout<T>.size))
-		data.withUnsafeMutableBufferPointer {
-			UnsafeMutableRawPointer($0.baseAddress!).storeBytes(of: value, as: T.self)
-		}
-		return data
+extension ComputeWorker: WebSocketClientDelegate {
+	public func onText(text: String) {
+		logger.warning("compute worker received unexpected text from server");
 	}
+	
+	public func onBinary(data: Data) {
+		delegate?.handleCompute(data: data)
+	}
+	
+	public func onPing(data: Data) {
+		wsclient.pong(data: data)
+	}
+	
+	public func onPong(data: Data) {
+		wsclient.ping(data: data)
+	}
+	
+	public func onClose(channel: Channel, data: Data) {
+		delegate?.handleConnectionClosed()
+	}
+	
+	public func onError(error: Error?, status: HTTPResponseStatus?) {
+		logger.warning("computer socket error: \(error?.localizedDescription ?? "?") for status: \(status?.reasonPhrase ?? "-")")
+		delegate?.handleCompute(error: .network)
+	}
+	
+	
 }
